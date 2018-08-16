48749a6e898b19d070047ef8b35dd78f0590ec73
Karol Chrapek <kchrapek@novomatic-tech.com>
2018-08-16 13:32:46 +0200

    Update test value and small role update

--- .travis/image.sh
+++ .travis/image.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-dd if=/dev/zero of=lvmtest0.img bs=200 count=1M
-sudo losetup /dev/loop0 lvmtest0.img
 for i in centos-molecule:7
 do
     docker pull paulfantom/$i &
--- tests/test_default.py
+++ tests/test_default.py
@@ -1,6 +1,5 @@
 import pytest
 from testinfra.utils.ansible_runner import AnsibleRunner
-
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
@@ -12,3 +11,13 @@ def test_directories(host, dirs):
     d = host.file(dirs)
     assert d.is_directory
     assert d.exists
+
+
+@pytest.mark.parametrize("mounts", [
+    "/var/lib/docker",
+    "/var/lib/test2"
+])
+def test_mounts(host, mounts):
+    m = host.mount_point(mounts)
+    assert m.exists
+    assert m.filesystem == 'xfs'
